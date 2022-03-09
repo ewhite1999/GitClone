1df299bc64357760454425cfae2a54780c4883d9
ewhite1999 <ewhite1999@gmail.com>
2022-03-09 16:19:50 +0000

    mobile styling for Profile component

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -2,11 +2,20 @@ import React from "react";
 const Profile = (props) => {
   const { login, avatar_url, html_url, name } = props.data;
   return (
-    <div className="flex space-x-5 p-5 ">
-      <img src={avatar_url} className="rounded-full w-16" alt="git avatar" />
+    <div className="flex space-x-5 p-5 items-center">
+      <img
+        src={avatar_url}
+        className="rounded-full w-14 h-14"
+        alt="git avatar"
+      />
       <div>
         {name && <h2 className="font-bold text-2xl">{name}</h2>}
-        <a className="text-xl" href={html_url} rel="noopener noreferrer" target='_blank'>
+        <a
+          className="text-xl"
+          href={html_url}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           {login}
         </a>
       </div>
