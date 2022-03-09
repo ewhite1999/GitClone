3f02d9715d54c898df5bcdea1227828a59527e3a
ewhite1999 <ewhite1999@gmail.com>
2022-03-09 16:29:19 +0000

    flex auto on ul

--- src/Components/RepoList/index.js
+++ src/Components/RepoList/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import Repo from "../Repo";
 const RepoList = (props) => {
   const reposArr = props.data;
-  console.log("this is the ReposArr", reposArr)
+  console.log("this is the ReposArr", reposArr);
   return (
-    <ul>
+    <ul className="md:flex-auto">
       {reposArr.map((r) => (
         <Repo key={r.id} data={r} />
       ))}
