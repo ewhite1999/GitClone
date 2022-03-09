91a1e90e52c6d335a5df01579b61ee6934b46497
ewhite1999 <ewhite1999@gmail.com>
2022-03-09 11:10:17 +0000

    add searchForm tests

---
+++ src/Components/SearchForm/SearchFrom.spec.js
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import SearchForm from ".";
+
+describe("form component", () => {
+  beforeEach(() => {
+    render(<SearchForm />);
+  });
+
+  it("renders a form", () => {
+    const form = screen.queryByRole("form");
+    expect(form).not.toBe(null);
+  });
+
+  it("lets you input a github username", () => {
+    const nameInput = screen.queryByLabelText(/username/i);
+    expect(nameInput).not.toBe(null);
+  });
+
+  it("the username is required", () => {
+    const nameInput = screen.queryByLabelText(/username/i);
+    expect(nameInput.required).toBe(true);
+  });
+
+  it("has a submit button", () => {
+    const submit = screen.queryByRole("button");
+    expect(submit).not.toBe(null);
+  });
+});
