c8e5f693d11025b87238e849caadca7415c183bc
ewhite1999 <ewhite1999@gmail.com>
2022-03-09 12:33:05 +0000

    add profile component

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -1,6 +1,13 @@
 import React from "react";
-const Profile = () => {
-  return <div>Hi</div>;
+const Profile = (props) => {
+  const { login, avatar_url, html_url, name } = props.data;
+  return (
+    <div>
+      <img src={avatar_url} />
+      {name && <h2>{name}</h2>}
+      <a href={html_url}>{login}</a>
+    </div>
+  );
 };
 
 export default Profile;
