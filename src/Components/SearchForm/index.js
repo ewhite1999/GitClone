be3719910d4911c183b3fa3071922f4b4e3e9b0a
Dafe-akaka <dafe.akaka123@gmail.com>
2022-03-09 12:57:33 +0000

    form complete

--- src/Components/SearchForm/index.js
+++ src/Components/SearchForm/index.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { RepoList } from "..";
 
-const SearchForm = () => {
+function SearchForm() {
+  const [username, setUsername] = useState("");
+  const [repoInfo, setRepoInfo] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const {data} = await axios.get(`https://api.github.com/users/${username}/repos`)
+    
+    setRepoInfo(data)
+  };
+  
+  useEffect(() => {
+    handleSubmit()
+  }, [username]);
+
+  <RepoList props= {repoInfo}/>
+
   return (
-    <div>SearchForm</div>
-  )
+    <form role="form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Github Username"
+        aria-label="username"
+        value = {username}
+        required
+        onChange={(e)=> setUsername(e.target.value)}
+      />
+      <input type="submit" value="Get Repos" />
+    </form>
+  );
 }
 
-export default SearchForm
+export default SearchForm;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from "./store";
-import { Provider } from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
-</React.StrictMode>,
-document.getElementById("root")
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
