7ebe6494bc5806a1994a1d824a2069e997fc0195
ewhite1999 <ewhite1999@gmail.com>
2022-03-09 11:39:30 +0000

    return list of repos form data

--- src/Components/RepoList/index.js
+++ src/Components/RepoList/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
-
-const RepoList = () => {
+import React from "react";
+import Repo from "../Repo";
+const RepoList = (props) => {
+  const reposArr = props.data;
   return (
-    <div>RepoList</div>
-  )
-}
+    <ul>
+      {reposArr.map((r) => (
+        <Repo key={r.id} data={r} />
+      ))}
+    </ul>
+  );
+};
 
-export default RepoList
+export default RepoList;
