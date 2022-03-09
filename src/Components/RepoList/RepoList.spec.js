d94a073cbeeb3986054f1e42e54fed5f8b60cbc4
ewhite1999 <ewhite1999@gmail.com>
2022-03-09 11:07:55 +0000

    add repoList tests

---
+++ src/Components/RepoList/RepoList.spec.js
@@ -0,0 +1,43 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import RepoList from ".";
+describe("Repo component", () => {
+  let repoList;
+  let reposArr;
+  beforeAll(() => {
+    reposArr = [
+      {
+        name: "3-column-preview-card-component-main",
+        description: "A task from frontend mentor",
+        stargazers_count: 0,
+        language: "HTML",
+        forks_count: 0,
+        open_issues_count: 0,
+      },
+      {
+        name: "testData",
+        description: "A test",
+        stargazers_count: 2,
+        language: "HTML",
+        forks_count: 5,
+        open_issues_count: 6,
+      },
+    ];
+  });
+
+  beforeEach(() => {
+    render(<RepoList data={reposArr} />);
+    repoList = screen.queryByRole("list");
+  });
+
+  it("returns an unordered list", () => {
+    expect(repoList).toBeTruthy();
+  });
+
+  it("has two child elements of li", () => {
+    const listItems = screen.getAllByRole("listitem");
+    expect(listItems.length).toBe(2);
+  });
+});
