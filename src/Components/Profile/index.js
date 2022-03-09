909bbdfce76871da9c9656386c7c8a7abad32c60
Dafe-akaka <dafe.akaka123@gmail.com>
2022-03-09 16:52:55 +0000

    style form

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -3,7 +3,7 @@ const Profile = (props) => {
   const { login, avatar_url, html_url, name } = props.data;
   return (
     <div className="flex space-x-5 p-5 ">
-      <img src={avatar_url} className="rounded-full w-16" alt="git avatar" />
+      {avatar_url && <img src={avatar_url} className="rounded-full w-16" alt="git avatar" />}
       <div>
         {name && <h2 className="font-bold text-2xl">{name}</h2>}
         <a className="text-xl" href={html_url} rel="noopener noreferrer" target='_blank'>
--- src/Pages/RepoPage/index.js
+++ src/Pages/RepoPage/index.js
@@ -5,42 +5,55 @@ import axios from "axios";
 import { RepoList } from "../../Components";
 
 const RepoPage = () => {
-
   const [username, setUsername] = useState("");
   const [repoInfo, setRepoInfo] = useState([]);
   const [userData, setUserData] = useState({});
+  
+  
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    const [repoData, userInfo] = await Promise.all ([
-        axios.get(`https://api.github.com/users/${username}/repos`), 
-        axios.get(`https://api.github.com/users/${username}`)
-    ])
-    
-    setRepoInfo(repoData.data)
-    setUserData(userInfo.data)
-    setUsername('')
+   e.preventDefault();
+   const [repoData, userInfo] = await Promise.all([
+     axios.get(`https://api.github.com/users/${username}/repos`),
+     axios.get(`https://api.github.com/users/${username}`),
+   ]);
 
-    console.log("thus is the repoData" , repoData)
-    console.log("thus is the userInfo" , userInfo)
-  };
+   setRepoInfo(repoData.data);
+   setUserData(userInfo.data);
+   setUsername("");
+
+   console.log("thus is the repoData", repoData);
+   console.log("thus is the userInfo", userInfo);
+ };
+//   useEffect(() => {
+
+//   }, [])
+  
 
   return (
     <>
-        <form role="form" onSubmit={ handleSubmit}>
-            <input
-                type="text"
-                placeholder="Github Username"
-                aria-label="username"
-                value = {username}
-                required
-                onChange={(e)=> setUsername(e.target.value)}
-                className='text-black'
+        <div className="flex justify-center flex-wrap mt-8">
+            <form className=" w-full max-w-sm" role="form" onSubmit={handleSubmit}>
+                <div className="flex items-center border-b border-blue-500 py-2">
+                <input
+                    className="appearance-none bg-transparent border-none w-full text-white-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    type="text"
+                    placeholder="Github Username"
+                    aria-label="username"
+                    value={username}
+                    required
+                    onChange={(e) => setUsername(e.target.value)}
                 />
-            <input type="submit" value="Get Repos" />
-        </form>
+                <input
+                    className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+                    type="submit"
+                    value="Get Repos"
+                />
+                </div>
+            </form>
+      </div>
       <Components.Profile data={userData}></Components.Profile>
-      <RepoList data={repoInfo}/>
+      <RepoList data={repoInfo} />
     </>
   );
 };
