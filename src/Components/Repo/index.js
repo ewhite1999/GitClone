454b79869ea491d9c1a76d29e8f8dcb810b0c5c6
ewhite1999 <ewhite1999@gmail.com>
2022-03-09 16:19:25 +0000

    mobile styling for repo componant

--- src/Components/Repo/index.js
+++ src/Components/Repo/index.js
@@ -12,16 +12,21 @@ const Repo = (props) => {
   } = props.data;
   return (
     <li>
-      <div className=" border-b-2">
-        <a href={html_url} rel="noopener noreferrer" target="_blank">
+      <div className=" border-b mx-4 py-6 space-y-2">
+        <a
+          className=" text-blue-300 font-bold text-xl"
+          href={html_url}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           {name}
         </a>
-        <p>{description}</p>
-        <div>
+        <p className=" text-sm">{description}</p>
+        <div className="flex text-xs space-x-5">
           <span>{language}</span>
-          {stargazers_count > 0 && <span>stargazers:{stargazers_count}</span>}
-          {forks_count > 0 && <span>forks:{forks_count}</span>}
-          {open_issues_count > 0 && <span>issues:{open_issues_count}</span>}
+          {stargazers_count > 0 && <span>stargazers: {stargazers_count}</span>}
+          {forks_count > 0 && <span>forks: {forks_count}</span>}
+          {open_issues_count > 0 && <span>issues: {open_issues_count}</span>}
         </div>
       </div>
     </li>
